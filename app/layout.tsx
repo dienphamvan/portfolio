52258b96c1e3d08344c21c2945a23dc74c6b6802
dienpv <chuanthanh133@gmail.com>
2025-06-19 00:07:10 +0700

    fix: change title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,19 +2,17 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+    title: 'Dien Pham Van Portfolio',
 }
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode
+    children: React.ReactNode
 }>) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  )
+    return (
+        <html lang='en'>
+            <body>{children}</body>
+        </html>
+    )
 }
